b4a9dcc14487af54c93517bf8a3dc33bd6bb43ba
Ryan <evercyan@qq.com>
2024-01-16 15:12:44 +0800

    no message

--- xjson/util.go
+++ xjson/util.go
@@ -16,33 +16,29 @@ var (
 	Decode = xencoding.JSONDecode
 )
 
-// Pretty ...
-func Pretty(v interface{}) string {
+// format ...
+func format(v interface{}) interface{} {
 	if vv, ok := v.(string); ok {
 		if vvv, err := strconv.Unquote(vv); err == nil {
 			vv = vvv
 		}
-		var raw json.RawMessage
+		var raw interface{}
 		if err := json.Unmarshal([]byte(vv), &raw); err == nil {
 			v = raw
 		}
 	}
-	b, _ := json.MarshalIndent(v, "", strings.Repeat(" ", 4))
+	return v
+}
+
+// Pretty ...
+func Pretty(v interface{}) string {
+	b, _ := json.MarshalIndent(format(v), "", strings.Repeat(" ", 4))
 	return string(b)
 }
 
 // Minify ...
 func Minify(v interface{}) string {
-	if vv, ok := v.(string); ok {
-		if vvv, err := strconv.Unquote(vv); err == nil {
-			vv = vvv
-		}
-		var raw json.RawMessage
-		if err := json.Unmarshal([]byte(vv), &raw); err == nil {
-			v = raw
-		}
-	}
-	return Encode(v)
+	return Encode(format(v))
 }
 
 // Format 格式化成可用 JSON 字符串
