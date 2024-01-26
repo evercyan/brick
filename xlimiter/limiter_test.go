5a62cf2c81c481300c0cc87cdd243fbfad64b8b9
Ryan <evercyan@qq.com>
2024-01-26 14:16:21 +0800

    no message

--- xlimiter/limiter_test.go
+++ xlimiter/limiter_test.go
@@ -6,17 +6,17 @@ import (
 	"testing"
 )
 
-func TestLimiters(t *testing.T) {
-	l := NewLimiters(2, 2)
+func TestLimiter(t *testing.T) {
+	// qps 限制为 2, 最大容量也为 2
+	lim := New(2, 2)
 	wg := new(sync.WaitGroup)
 	wg.Add(10)
+	// 起 10 个协程请求, 预期只输出 2 个 "execute"
 	for i := 0; i < 10; i++ {
 		go func() {
 			defer wg.Done()
-			if l.Allow() {
-				fmt.Println("1")
-			} else {
-				fmt.Println("0")
+			if lim.Allow() {
+				fmt.Println("execute")
 			}
 		}()
 	}
